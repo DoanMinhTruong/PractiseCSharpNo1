9b6cf5b08219d7bf66408e0905c887694cf6cc75
Meir Truong <meir.truong.2309@gmail.com>
2022-01-12 17:48:53 +0700

    update View Jewel

--- Jewel/Main.Designer.cs
+++ Jewel/Main.Designer.cs
@@ -29,9 +29,6 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.cbbCategory = new System.Windows.Forms.ComboBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.btnAddJewel = new System.Windows.Forms.Button();
             this.No = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.price = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -39,6 +36,9 @@
             this.created_by = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.image = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.action = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.cbbCategory = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.btnAddJewel = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -58,37 +58,6 @@
             this.dataGridView1.Size = new System.Drawing.Size(897, 498);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
-            // 
-            // cbbCategory
-            // 
-            this.cbbCategory.FormattingEnabled = true;
-            this.cbbCategory.Location = new System.Drawing.Point(12, 12);
-            this.cbbCategory.Name = "cbbCategory";
-            this.cbbCategory.Size = new System.Drawing.Size(121, 21);
-            this.cbbCategory.TabIndex = 1;
-            this.cbbCategory.SelectedIndexChanged += new System.EventHandler(this.cbbCategory_SelectedIndexChanged);
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBox1.Location = new System.Drawing.Point(211, 10);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(142, 22);
-            this.checkBox1.TabIndex = 2;
-            this.checkBox1.Text = "Only Your Jewels";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
-            // 
-            // btnAddJewel
-            // 
-            this.btnAddJewel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAddJewel.Location = new System.Drawing.Point(662, 5);
-            this.btnAddJewel.Name = "btnAddJewel";
-            this.btnAddJewel.Size = new System.Drawing.Size(152, 37);
-            this.btnAddJewel.TabIndex = 3;
-            this.btnAddJewel.Text = "Management";
-            this.btnAddJewel.UseVisualStyleBackColor = true;
             // 
             // No
             // 
@@ -130,6 +99,37 @@
             this.action.Text = "View";
             this.action.UseColumnTextForButtonValue = true;
             this.action.Width = 50;
+            // 
+            // cbbCategory
+            // 
+            this.cbbCategory.FormattingEnabled = true;
+            this.cbbCategory.Location = new System.Drawing.Point(12, 12);
+            this.cbbCategory.Name = "cbbCategory";
+            this.cbbCategory.Size = new System.Drawing.Size(121, 21);
+            this.cbbCategory.TabIndex = 1;
+            this.cbbCategory.SelectedIndexChanged += new System.EventHandler(this.cbbCategory_SelectedIndexChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(211, 10);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(142, 22);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Only Your Jewels";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // btnAddJewel
+            // 
+            this.btnAddJewel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddJewel.Location = new System.Drawing.Point(662, 5);
+            this.btnAddJewel.Name = "btnAddJewel";
+            this.btnAddJewel.Size = new System.Drawing.Size(152, 37);
+            this.btnAddJewel.TabIndex = 3;
+            this.btnAddJewel.Text = "Add a Jewel";
+            this.btnAddJewel.UseVisualStyleBackColor = true;
             // 
             // mainForm
             // 
